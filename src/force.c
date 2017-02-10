7e526ffbd1b92b1f735bdebc476972ae049e258f
Giangiacomo Sanna <giangiacomo.sanna@gmail.com>
2017-02-10 13:49:12 +0100

    force.c avoid expensive math operations

--- src/force.c
+++ src/force.c
@@ -5,9 +5,15 @@
 
 void force(mdsys_t *sys)
 {
-    double r,ffac;
+    double rsq,rinv,r6,ffac;
     double rx,ry,rz;
     int i,j;
+
+    /* pre-compute expensive operations whose result is used inside the loop */
+    double c12=4.0*sys->epsilon*pow(sys->sigma,12.0);
+    double c6=4.0*sys->epsilon*pow(sys->sigma, 6.0);
+    double rcsq = sys->rcut * sys->rcut;
+    double half_box = 0.5*sys->box;
 
     /* zero energy and forces */
     sys->epot=0.0;
@@ -16,28 +22,39 @@ void force(mdsys_t *sys)
     azzero(sys->fz,sys->natoms);
 
     for(i=0; i < (sys->natoms); ++i) {
+/* if not using openmp, only loop over pairs and update
+ * the force of two particles at each iteration */
+#ifndef _OPENMP
+        for(j=i+1; j < (sys->natoms); ++j) {
+#else
         for(j=0; j < (sys->natoms); ++j) {
-
             /* particles have no interactions with themselves */
             if (i==j) continue;
+#endif
 
             /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
-            ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-            rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            r = sqrt(rx*rx + ry*ry + rz*rz);
+            rx=pbc(sys->rx[i] - sys->rx[j], half_box);
+            ry=pbc(sys->ry[i] - sys->ry[j], half_box);
+            rz=pbc(sys->rz[i] - sys->rz[j], half_box);
 
             /* compute force and energy if within cutoff */
-            if (r < sys->rcut) {
-                ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-                                         +6*pow(sys->sigma/r,6.0)/r);
+            rsq = rx*rx + ry*ry + rz*rz;
+            if (rsq < rcsq) {
+                rinv=1.0/rsq;
+                r6=rinv*rinv*rinv;
 
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-                                               -pow(sys->sigma/r,6.0));
+                ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
+                sys->epot += r6*(c12*r6 - c6);
 
-                sys->fx[i] += rx/r*ffac;
-                sys->fy[i] += ry/r*ffac;
-                sys->fz[i] += rz/r*ffac;
+                sys->fx[i] += rx*ffac;
+                sys->fy[i] += ry*ffac;
+                sys->fz[i] += rz*ffac;
+
+#ifndef _OPENMP
+                sys->fx[j] += -rx*ffac;
+                sys->fy[j] += -ry*ffac;
+                sys->fz[j] += -rz*ffac;
+#endif
             }
         }
     }
